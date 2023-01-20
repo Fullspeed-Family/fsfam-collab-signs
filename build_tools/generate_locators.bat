11a5942343c21a4edaa50b3e98ac584063e15a2b
7thDoomsday <johannes_gerbig@hotmail.de>
2023-01-21 00:12:19 +0100

    add signs and locs

--- build_tools/generate_locators.bat
+++ build_tools/generate_locators.bat
@@ -1,8 +1,10 @@
 Rem credits to https://github.com/mariejuku
-set host=https://c-scholz.github.io/ritual-signpack/<path_to_sources>/
+set host=https://fullspeed-family.github.io/fsfam-collab-signs/Advertisement6x1/FSFam_collab_signs/
 del *.tga.loc
 del *.png.loc
 del *.webm.loc
+del *.jpg.loc
 for /r %%a in (*.tga) do @echo %host%%%~nxa > %%~nxa.loc
 for /r %%a in (*.png) do @echo %host%%%~nxa > %%~nxa.loc
 for /r %%a in (*.webm) do @echo %host%%%~nxa > %%~nxa.loc
+for /r %%a in (*.jpg) do @echo %host%%%~nxa > %%~nxa.loc
